eecd1c0ce6edf7e62aa3cfceec02a4ffbafbc0d3
Kevin Prakasa <kevinprakasa15@gmail.com>
2020-02-14 01:11:17 -0800

    Initial Commit

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Kevin Prakasa on 2/14/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Kevin Prakasa on 2/12/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
